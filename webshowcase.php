42634b87ef6743c501c19b415307330c83399922
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2023-01-09 16:55:01 -0800

    add comments

--- webshowcase.php
+++ webshowcase.php
@@ -16,18 +16,23 @@
 
  */
 
+/**
+  * Some initialization. The internationalization mechanism is almost put in place but
+  * not enabled yet
+*/
+
 function webshowcase_dynamic_init() {
   require_once(ABSPATH . 'wp-admin/includes/media.php');
   require_once(ABSPATH . 'wp-admin/includes/file.php');
   require_once(ABSPATH . 'wp-admin/includes/image.php');
 
-/*
-   wp_register_script(
+  /*
+    wp_register_script(
     'croquet-showcase',
      __DIR__ . '/build',
      array('wp-i18n')
-   );
-*/
+  );
+  */
 
   register_block_type( __DIR__ . '/build', array(
     'render_callback' => 'webshowcase_dynamic_render_callback'));
@@ -45,40 +50,13 @@ add_action('init', 'webshowcase_dynamic_init');
 add_action('plugins_loaded', 'showcase_load_plugin_textdomain');
 */
 
-function get_attachment_by_name($html_name) {
-   $args = array(
-     'posts_per_page' => 1,
-     'post_type'      => 'attachment',
-     'name'           => trim( $html_name ));
-
-   $get_attachment = new WP_Query( $args );
-
-   if (! $get_attachment || ! isset( $get_attachment->posts, $get_attachment->posts[0])) {
-     return false;
-   }
-
-   return $get_attachment->posts[0];
-}
-
-function delete_if__exists($contents, $html_name) {
-   $prev = get_attachment_by_name($html_name);
-   if ($prev) {
-     $file = file_get_contents($prev->guid);
-     /*
-     do_action("qm/debug", $file);
-     do_action("qm/debug", $contents);
-     do_action("qm/debug", 'equal: ' . ($file == $contents));
-     */
-
-     if ($file && $file == $contents) {return $prev->guid;}
-
-     // I want to know how to get the previous content and delete it only if necessary
-     $id = $prev->ID;
-
-     wp_delete_attachment($id, true);
-   }
-   return false;
-}
+/**
+  * The entry point of the callback. It generates a simple HTML file by concatenating fragments
+  * based on the JSON string that represents the list of "cards".
+  * Check if the generated HTML is different so that it has to be update in the media library.
+  * Get the accessible URL for the HTML in the Media Library, and returns an HTML fragement
+  * that uses the URL as src for an iframe.
+*/
 
 function webshowcase_dynamic_render_callback( $block_attributes, $content ) {
   $contents1 = <<<SHOWCASE
@@ -165,4 +143,39 @@ SHOWCASE;
   do_action("qm/debug", '$src: ' . $src);
 
   return '<div class="showcase-container"><iframe width="100%" height=' . $minHeight . ' class="showcase-iframe" src="' . $src . '?q=' . $sanitizedName . '#pw=1"></iframe></div>';
+}
+
+function get_attachment_by_name($html_name) {
+   $args = array(
+     'posts_per_page' => 1,
+     'post_type'      => 'attachment',
+     'name'           => trim( $html_name ));
+
+   $get_attachment = new WP_Query( $args );
+
+   if (! $get_attachment || ! isset( $get_attachment->posts, $get_attachment->posts[0])) {
+     return false;
+   }
+
+   return $get_attachment->posts[0];
+}
+
+function delete_if__exists($contents, $html_name) {
+   $prev = get_attachment_by_name($html_name);
+   if ($prev) {
+     $file = file_get_contents($prev->guid);
+     /*
+     do_action("qm/debug", $file);
+     do_action("qm/debug", $contents);
+     do_action("qm/debug", 'equal: ' . ($file == $contents));
+     */
+
+     if ($file && $file == $contents) {return $prev->guid;}
+
+     // I want to know how to get the previous content and delete it only if necessary
+     $id = $prev->ID;
+
+     wp_delete_attachment($id, true);
+   }
+   return false;
 }
